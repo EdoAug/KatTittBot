4e70cae15fe12fd18e40d852241f0debb8a0ead9
EdoAug <edvardhaaland@gmail.com>
2025-07-26 21:46:14 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const { Mwn } = require('mwn');
 const auth = require('./auth.json');
+
 const sandbox = 'User:KatTittBot/sandkasse';
 const language = "no"
 
@@ -30,16 +31,111 @@ const bot = new Mwn({
 	maxRetries: 5
 });
 
+/*{
+	"action": "query",
+	"format": "json",
+	"prop": "revisions",
+	"titles": page,
+	"redirects": 1,
+	"formatversion": "2",
+	"rvlimit": "1",
+	"rvdir": "newer"
+}*/
+
 bot.login().then(() => {
 	console.log("Logged in!");
 	//TestEdit();
-	MostPopular(20, sandbox ).then((res) => {
+	
+	MostPopular(20, sandbox, 1).then((res) => {
 		for (let i = 0; i < res.length; i++) {
 			res[i] = "{{Bruker:EdoAug/FFTT/Mal:Artikkellenke|artikkel="+res[i]+"}}";
 		}
-		bot.save(sandbox, res.join(" "), 'Legger inn liste over mest leste sider');
+		bot.save(sandbox, res.join(" "), 'Legger inn eksempelsformatert liste over mest leste sider basert på nyeste tilgjengelig statistikkdump.');
 	});
+	const cattygory = "Kategori:Brett- og kortspill";
+	RedLinksTestTwo(cattygory, "0").then((res) => {
+		let page = new bot.Page("User:KatTittBot/test");
+		console.log(res);
+		/*const gaggoo = res;
+		gaggoo = Object.entries(...gaggoo);
+		res = gaggoo.filter(x => x[1] > 1);*/
+		let prepend = '{| class="wikitable sortable" \n|+ Gjentatte røde lenker under [[:'+cattygory+'|'+cattygory.replace("Kategori:","")+']]\n! Lenke !! Antall\n|-\n|';
+		let append = '\n|}';
+		res = Object.entries(res).map(x => "[["+x[0]+"]]||"+x[1] ).join("\n|-\n|").toString();
+		page.save(prepend+res+append).catch((err) => console.log("Could not create page:",err));
+	});
+	
+	/*RedLinksInCategoryTree("Brett- og kortspill", "0").then((res) => {
+		//console.log(res.sort());
+		TitleCounter(res).then((res) => {
+			console.table(res);
+		})
+	}).catch((err) => {console.log(err)});*/
+	
 });
+
+async function RedLinksTestTwo(category="Kategori:Skeive emner", namespace) {
+	return new Promise((resolve) => {
+		GetCategoryTree(category).then((res) => {
+			return res;
+		}).then((res) => {
+			const arr = [];
+			const arr2 = [];
+			bot.batchOperation(
+			res,
+			(page, idx) => {
+				return new Promise((resolbe) => {
+					bot.query({
+					"action": "query",
+					"format": "json",
+					"list": "categorymembers",
+					"formatversion": "2",
+					"cmtitle": page,
+					"cmlimit": "max"
+					}).then((res) => {
+						arr.push(res);
+						resolbe(res);
+					}).catch((err) => console.log(err));
+				}).then((res) => {
+					return res;
+				});
+			},
+			/* concurrency */ 10,
+			/* retries */ 2
+			).then(() => {
+				const pagelist = arr.flatMap(obj => obj.query.categorymembers);
+				const pagelistunique = [...new Set(pagelist.map(obj => obj.title))];
+				bot.batchOperation(
+				pagelistunique,
+				(page) => {
+					return new Promise((resolbe) => {
+						bot.request({
+							"action": "parse",
+							"format": "json",
+							"page": page,
+							"prop": "links",
+							"formatversion": "2"
+						}).then((res) => {
+							let linkies = res.parse.links.filter((x) => !x.exists);
+							if (namespace) {
+								linkies = linkies.filter((x) => x.ns == namespace);
+							}
+							return linkies;
+						}).then((res) => resolbe(res)).catch((err) => console.log(err));
+					}).then((res) => arr2.push(res));
+				},
+				/* concurrency */ 10,
+				/* retries */ 2).then(() => {
+					let redLinks = Array.from(arr2.flat(), obj => obj.title);
+					TitleCounter(redLinks).then((res) => {
+						resolve(res);
+					})
+				});
+			});
+		}).catch((err) => console.log(err));
+	});
+}
+
 
 function TestEdit() {
 	console.log("Initiating TestEdit...");
@@ -95,6 +191,188 @@ async function MostPopular(amount = 4, page = sandbox, days = 2) {
 					}
 				}
 				resolve(topArticles);
+			}).catch((err) => console.log(err));
+	});
+}
+
+async function TitleCounter(arr) {
+	return new Promise((resolve) => {
+		const sorts = {};
+		
+		for (let i = 0; i < arr.length; i++) {
+			let label = i.toString().replace("_", " ");
+			if (!(arr[label] in sorts)) {
+				sorts[arr[label]] = 1;
+			}
+			else {
+				sorts[arr[label]]++;
+			}
+			if (i == arr.length-1) {
+				const sorted = Object.fromEntries(
+					Object.entries(sorts).sort(([, a], [, b]) => b - a)
+				);
+				resolve(sorted);
+			}
+		}
+	});
+}
+
+async function RedLinksInCategoryTree(category, namespace) {
+	return new Promise((resolve) => {
+		GetCategoryTree(category).then((res) => {
+			PagesInCategories(res).then((res) => {
+				res = res.flat();
+				if (namespace) {
+					//console.log(namespace);
+					res = res.filter((obj) => obj.ns == namespace);
+				}
+				else {
+					namespace = "*";
+				}
+				console.log("Initiating red link hunt within",category,"(NS:"+namespace+")");
+				res = res.map(obj => obj.title);
+				bot.massQuery({
+					"action": "query",
+					"format": "json",
+					"prop": "links",
+					"titles": res,
+					"formatversion": "2",
+					"plnamespace": namespace,
+					"pllimit": "max"
+				}).then((res) => {
+					console.log("Grabbed all links...");
+					res = res.flatMap(obj => obj.query.pages).flatMap(obj => obj.links)
+					SplitArrayIntoChunks(res).then((res) => {
+						console.log("Weeding out blues... (be patient!)");
+						let i = 0;
+						const checked = [];
+						const titles = [];
+						let count = 0;
+						res.forEach((list) => {
+							count++;
+							console.log(count);
+							let t = 0;
+							list.forEach((item) => {
+								if (!item) {
+									t++;
+									return;
+								}
+								let title = new bot.Page(item.title);
+								//console.log(count, title);
+								if (titles.includes(title.title)){
+									titles.push(title.title);
+									t++;
+									return;
+								}
+								else if (checked.includes(title.title)){
+									t++;
+									return;
+								}
+								//console.log("Checking",title.title);
+								title.exists().then((exists) => {
+									console.log(i,"of",res.length-1, "—", t,"of",list.length-1,"–",title.title,"("+exists+")");
+									if (!exists) {
+										titles.push(title.title);
+										//console.log(titles);
+									}
+									else {
+										//console.log("– BLUE:",title.title);
+									}
+									//console.log(i);
+								}).then(() => {
+									if ((i >= res.length-1) && (t >= list.length-1)) {
+										console.log("Gathered",titles.length,"red links!");
+										resolve(titles);
+									}
+									else if (t >= list.length-1) {
+										i++;
+										return;
+									}
+									else {
+										t++;
+									}
+								}).catch((err) => console.log(err));
+							});
+						});
+					}).catch((err) => console.log(err));
+					//res = res.flatMap(obj => obj.query.pages).map(obj => obj.links);
+					//res = res.query.pages.flatMap(obj => obj.links).map(obj => obj.title)
+				}).catch((err) => {
+					//bot.save("Bruker:KatTittBot/test",err);
+					console.log(err);
+				});
+			}).catch((err) => console.log(err));
+		});
+	})
+}
+
+async function SplitArrayIntoChunks(arr) {
+	return new Promise((resolve) => {
+		let results = [];
+		let i = Math.ceil(arr.length / 450);
+		if (i>16) {
+			console.log("Too many links for me at this time! Shortening it!")
+			i = 16;
+		}
+		for (let t = 0; t < i; t++) {
+			results.push(arr.slice(0,450));
+			arr.splice(0, 450);
+			console.log(t, "out of", i-1, "arrays split.");
+			if (t == i-1) {
+				console.log("All arrays chopped up into bite-size pieces! Yummy!");
+				resolve(results);
+			}
+		}
+	});
+}
+
+function GetCategoryTree(category, depth=1) {
+	const catRegex = /K\w+:[\w\_\d\sÆØÅæøå\-\(\)]+/g;
+	return new Promise((resolve) => {
+		bot.request({
+			"action": "categorytree",
+			"format": "json",
+			"category": category,
+			"options": "{\"depth\":"+depth.toString()+"}",
+			"formatversion": "2"
+		}).then((res) => {
+			let cats = JSON.stringify(res).match(catRegex);
+			if (!cats) {
+				cats = [category];
+			}
+			else {
+				for (let i = 0; i < cats.length; i++) {
+					cats.splice(i, 1);
+				}
+			}
+			cats.push(category);
+			resolve(cats);
+		});
+	});
+}
+
+async function MassExists(pages) {
+	return new Promise((resolve) => {
+		//
+	})
+}
+
+async function PagesInCategories(catArray) {
+	return new Promise((resolve) => {
+		const categories = [];
+		let t = 0;
+		/*for await (const cat of catArray) {
+			let category = 
+		}*/
+		catArray.forEach(cat => {
+			new bot.Category(cat).members().then((res) => {
+				categories.push(res);
+				//console.log(t,catArray.length-1);
+				if (t === catArray.length-1) {
+					resolve(categories);
+				}
+				t++;
 			});
+		});
 	});
 }
